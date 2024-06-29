ce89ba69c26c6b847ca437a8a8a3349ca4cb0ec3
Gustavo <guh.barbosa56@gmail.com>
2024-06-29 00:00:06 -0300

    Deploy do Projeto Blog

--- src/main/java/com/blog/blog/model/Usuario.java
+++ src/main/java/com/blog/blog/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -26,6 +27,7 @@ public class Usuario {
 	@NotBlank(message = "O atributo nome é obrigatorio!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "O atributo Usuário é Obrigatorio")
 	@Email(message = "O atributo Usuario deve ser um E-mail valido")
 	private String usuario;
@@ -40,6 +42,16 @@ public class Usuario {
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() { }
 
 	public Long getId() {
 		return id;
